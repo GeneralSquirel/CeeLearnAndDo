e6239c15330625a2c9caba391a02cd9d20b39be2
145379 <145379@student.horizoncollege.nl>
2021-03-16 11:07:51 +0100

    Created CeeLearnAndDo Project.

---
+++ CeeLearnAndDo_WijOnt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CeeLearnAndDo_WijOnt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
