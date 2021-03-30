e709a95f5c5e567553b52ea7c24dc89291264c2b
145379 <jelle.m.venema@gmail.com>
2021-03-30 08:52:35 +0200

    Added Models.

---
+++ CeeLearnAndDo_WijOnt/Models/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo_WijOnt.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public Consultant Consultant { get; set; }
+        public int ConsultantId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ CeeLearnAndDo_WijOnt/Models/Consultant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeeLearnAndDo_WijOnt.Models
+{
+    public class Consultant
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string UserId { get; set; }
+        public string ImageUrl { get; set; }
+        public string Biography { get; set; }
+    }
+}
