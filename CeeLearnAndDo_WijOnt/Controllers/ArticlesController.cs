3e6d4e0ee0917d18a045f5c7ed6a6993b292acc3
halil ibrahim aydin <aydinhalil24.ha@gmail.com>
2021-04-15 09:09:00 +0200

    Article pagina

---
+++ CeeLearnAndDo_WijOnt/Controllers/ArticlesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CeeLearnAndDo_WijOnt.Data;
+using CeeLearnAndDo_WijOnt.Models;
+
+namespace CeeLearnAndDo_WijOnt.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public ArticlesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Article()
+        {
+            var applicationDbContext = _context.Article.Include(a => a.Consultant);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Articles
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Article.Include(a => a.Consultant);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Articles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Article
+                .Include(a => a.Consultant)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // GET: Articles/Create
+        public IActionResult Create()
+        {
+            ViewData["ConsultantId"] = new SelectList(_context.Set<Consultant>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Articles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ConsultantId,Title,CreationDate,Content")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(article);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ConsultantId"] = new SelectList(_context.Set<Consultant>(), "Id", "Id", article.ConsultantId);
+            return View(article);
+        }
+
+        // GET: Articles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Article.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            ViewData["ConsultantId"] = new SelectList(_context.Set<Consultant>(), "Id", "Id", article.ConsultantId);
+            return View(article);
+        }
+
+        // POST: Articles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ConsultantId,Title,CreationDate,Content")] Article article)
+        {
+            if (id != article.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(article);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticleExists(article.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ConsultantId"] = new SelectList(_context.Set<Consultant>(), "Id", "Id", article.ConsultantId);
+            return View(article);
+        }
+
+        // GET: Articles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Article
+                .Include(a => a.Consultant)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var article = await _context.Article.FindAsync(id);
+            _context.Article.Remove(article);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Article.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CeeLearnAndDo_WijOnt/Migrations/20210414181649_Article.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CeeLearnAndDo_WijOnt.Migrations
+{
+    public partial class Article : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consultant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId1 = table.Column<int>(type: "int", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Biography = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultant", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultant_User_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Article",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConsultantId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Article", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Article_Consultant_ConsultantId",
+                        column: x => x.ConsultantId,
+                        principalTable: "Consultant",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Article_ConsultantId",
+                table: "Article",
+                column: "ConsultantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultant_UserId1",
+                table: "Consultant",
+                column: "UserId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Article");
+
+            migrationBuilder.DropTable(
+                name: "Consultant");
+        }
+    }
+}
